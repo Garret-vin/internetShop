6e7b9771d51b9cb67647a6ae6a95cd16d4fac317
Garret <pavlenko.av92@gmail.com>
2019-07-05 12:17:53 +0300

    day 2 after refactor structure

--- src/main/java/controller/UsersServlet.java
+++ src/main/java/controller/UsersServlet.java
@@ -11,12 +11,14 @@ import java.io.IOException;
 public class UsersServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         req.getRequestDispatcher("users.jsp").forward(req, resp);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         super.doPost(req, resp);
     }
 }
