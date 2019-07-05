a2a4bde2e5baded7951724cf59053e08ebb6ef3f
Garret <pavlenko.av92@gmail.com>
2019-07-05 13:07:47 +0300

    day 3 products page (Update 1)

--- src/main/java/controller/ProductsServlet.java
+++ src/main/java/controller/ProductsServlet.java
@@ -11,12 +11,8 @@ import java.io.IOException;
 public class ProductsServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         req.getRequestDispatcher("products.jsp").forward(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
     }
 }
